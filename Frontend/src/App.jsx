9ca20ba6d30db601429bbc336f2a35da67d56532
bluewater4 <nelaab.attiqi@gmail.com>
2026-07-15 19:10:53 -0400

    .

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import LoadTable from "./loadtable";
 import LoadShipments from "./loadshipments";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Manager from "./pages/Manager.jsx";
 import Sidebar from "./Sidebar";
 import "./App.css";
